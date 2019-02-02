a5c287378954b4c268b269ce32b92d42e25899f8
Zalmar <zalmardev@gmail.com>
2019-02-02 15:18:12 +0400

    fixes accept in headers

--- parser_weather.py
+++ parser_weather.py
@@ -7,8 +7,10 @@ from bs4 import BeautifulSoup
 
 def get_weather(city):
     weather = {}
-    headers = {'accept': '*/*', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-                                              '(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+    headers = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+               'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+
     url = f'https://yandex.ru/pogoda/{city}'
     while True:
         session = requests.Session()
